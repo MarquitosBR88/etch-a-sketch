f17a9c9768365778c35040b3a67917214cb0779f
Marcus <marcus.vi.mar@gmail.com>
2024-05-07 11:45:28 -0300

    Create newGrid function and other changes
    I did a major rework on the code, changed the flexbox to the grid to fit all the container no matter the quantity of blocks, changed a lot of the JavaScript code to be possible to create other grids without reloading the page, but I still need to figure out how to delete the former grid when you create another instead of appending to it.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,50 @@
+
+
 const container = document.querySelector(".container");
-for (i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
+const customGrid = document.querySelector("button");
+
+//Default value
+newGrid(16)
+
+function newGrid(value) {
+    gridCreator(value);
+    const grid = document.querySelectorAll(".children");
+    function colorChange(index) {
+        grid[index].addEventListener("mouseover", () => {
+            grid[index].style.background = "black"
+        }); 
+    }
+    //For each div iteration, add border and the colorChange function when hover 
+    for (i = 0; i < grid.length; i++) {
+        grid[i].style.border = "solid 1px black";
+        colorChange(i);
+    }
 }
 
-const grid = document.querySelectorAll(".grid");
+customGrid.addEventListener("click", () => {
+    let value = parseInt(prompt("How many rows?"));
+    newGrid(value);
+});
 
-for (i = 0; i < grid.length; i++) {
-    grid[i].style.border = "solid black";
-    colorChange(i);
+//Create the divs blocks of the grid
+function gridCreator(value) { 
+    for (i = 0; i < value; i++) {
+        x = 0
+        const rows = document.createElement("div");
+        rows.classList.add("rows", "grid");
+        container.appendChild(rows);
+        while (x < value) {
+            const childDivs = document.createElement("div");
+            childDivs.classList.add("children", "grid");
+            rows.appendChild(childDivs);
+            x++
+        }
+    }
 }
 
-function colorChange(index) {
-    grid[index].addEventListener("mouseover", () => {
-        grid[index].style.background = "black"
-    }); 
-}
+
+
+
 
 
 
